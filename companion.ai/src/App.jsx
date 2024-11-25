2544a12ed3012ce3e479cf6303d3bd999d155fc0
Zach Hatzenbeller <zhatz111@gmail.com>
2024-11-24 23:59:17 -0500

    starting on creating the actual website with what i learned from react tutorial

---
+++ companion.ai/src/App.jsx
@@ -0,0 +1,278 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <div>
+        <nav className="flex bg-indigo-700 border-b border-indigo-500">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div
+              className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+            >
+              {/* <!-- Logo --> */}
+              <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+                <img
+                  className="h-10 w-auto"
+                  src="images/logo.png"
+                  alt="React Jobs"
+                />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2"
+                  >React Jobs</span
+                >
+              </a>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <a
+                    href="/index.html"
+                    className="text-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >Home</a
+                  >
+                  <a
+                    href="/jobs.html"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >Jobs</a
+                  >
+                  <a
+                    href="/add-job.html"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    >Add Job</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* Side Bar Navigation */}
+      <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <span className="sr-only">Open sidebar</span>
+        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+      </button>
+
+      <aside id="default-sidebar" className="fixed top-20 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <ul className="space-y-2 font-medium">
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                    </svg>
+                    <span className="ms-3">Dashboard</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                        <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Kanban</span>
+                    <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
+                    <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                        <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Products</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
+                        <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
+                        <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
+                  </a>
+              </li>
+            </ul>
+        </div>
+      </aside>
+
+
+      {/* <!-- Hero --> */}
+      <section className="bg-indigo-700 py-20 mb-4">
+        <div
+          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+        >
+          <div className="text-center">
+            <h1
+              className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+            >
+              Become a React Dev
+            </h1>
+            <p className="my-4 text-xl text-white">
+              Find the React job that fits your skills and needs
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* <!-- Developers and Employers --> */}
+      <section className="py-4">
+        <div className="container-xl lg:container m-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+            <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+              <h2 className="text-2xl font-bold">For Developers</h2>
+              <p className="mt-2 mb-4">
+                Browse our React jobs and start your career today
+              </p>
+              <a
+                href="/jobs.html"
+                className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+              >
+                Browse Jobs
+              </a>
+            </div>
+            <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+              <h2 className="text-2xl font-bold">For Employers</h2>
+              <p className="mt-2 mb-4">
+                List your job to find the perfect developer for the role
+              </p>
+              <a
+                href="/add-job.html"
+                className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+              >
+                Add Job
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* <!-- Browse Jobs --> */}
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            Browse Jobs
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/* <!-- Job Listing 1 --> */}
+            <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Full-Time</div>
+                  <h3 className="text-xl font-bold">Senior React Developer</h3>
+                </div>
+
+                <div className="mb-5">
+                We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+                </div>
+
+                <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    Boston, MA
+                  </div>
+                  <a
+                    href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* <!-- Job Listing 2 --> */}
+            <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Remote</div>
+                  <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
+                </div>
+
+                <div className="mb-5">
+                Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
+                </div>
+
+                <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    Miami, FL
+                  </div>
+                  <a
+                    href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* <!-- Job Listing 3 --> */}
+            <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Remote</div>
+                  <h3 className="text-xl font-bold">React.js Developer</h3>
+                </div>
+
+                <div className="mb-5">
+                  Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
+                </div>
+
+                <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    Brooklyn, NY
+                  </div>
+                  <a
+                    href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
